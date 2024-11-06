77cdbb711aca9f52283ffffd0152cdcec8c75921
samio11 <samiohasan6@gmail.com>
2024-11-06 21:49:10 +0600

    Problem 1 solved

---
+++ problem-1.ts
@@ -0,0 +1,11 @@
+{
+    //
+    // solving problem 01
+    const sumArray = (values: number[]): number => {
+        const result = values.reduce((x, y) => x + y, 0);
+        return result;
+    }
+    sumArray([1, 2, 3, 4, 5]);
+    // console.log(sumArray([1, 2, 3, 4, 5]))
+    //
+}
